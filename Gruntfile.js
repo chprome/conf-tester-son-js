07f9e620c72be5d5e77cea04761c283e734fe9ff
nicolas <nmedda@viareport.com>
2013-11-20 17:12:41 +0100

    Ajout tâche grunt pour lancer les tests avec Karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-testem');
+    grunt.loadNpmTasks('grunt-karma');
 
     grunt.initConfig({
 
@@ -46,9 +47,54 @@ module.exports = function(grunt) {
                     "./test/integration/integrationTest.js"
                 ]
             }
+        },
+
+        karma: {
+            unit: {
+                options: {
+                    singleRun: true,
+                    files: [
+                        './js/Model.js',
+                        './test/unit/specs/ModelTest.js'
+                    ],
+                    frameworks: ['mocha', 'chai', 'sinon'],
+                    browsers: ['Chrome', 'Firefox'],
+                    autoWatch: true,
+                    plugins: [
+                        'karma-mocha',
+                        'karma-chai',
+                        'karma-sinon',
+                        'karma-chrome-launcher',
+                        'karma-firefox-launcher'
+                    ]
+                }
+            },
+            integration: {
+                options: {
+                    singleRun: true,
+                    files: [
+                        './js/Model.js',
+                        './js/View.js',
+                        './js/Controller.js',
+                        './js/App.js',
+                        './test/integration/integrationTest.js'
+                    ],
+                    frameworks: ['mocha', 'chai', 'sinon'],
+                    browsers: ['Chrome', 'Firefox'],
+                    autoWatch: true,
+                    plugins: [
+                        'karma-mocha',
+                        'karma-chai',
+                        'karma-sinon',
+                        'karma-chrome-launcher',
+                        'karma-firefox-launcher'
+                    ]
+                }
+            }
         }
     });
 
     grunt.registerTask('default', ['connect']);
-    grunt.registerTask('test', ['testem:ci:integration', 'testem:ci:unit']);
+    grunt.registerTask('test-testem', ['testem:ci:integration', 'testem:ci:unit']);
+    grunt.registerTask('test-karma', ['karma:integration', 'karma:unit']);
 };
